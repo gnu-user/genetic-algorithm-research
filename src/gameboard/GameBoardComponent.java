a1ca0a30621d97b7780fd50d8759a79e93cab243
gnu_user <gsoc.student@gmail.com>
2013-02-18 13:21:49 -0500

    Refactoring project layout, moved gameboard code into package

---
+++ src/gameboard/GameBoardComponent.java
@@ -0,0 +1,93 @@
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import java.awt.Color;
+import java.awt.Graphics;			//Used to create a 2D graphic
+import java.awt.Graphics2D;			//Used to Draw a Rectangle and a name
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.awt.image.ImageObserver;
+
+public class GameBoardComponent extends JComponent
+{
+	public static final Color PASS = Color.BLACK;
+	public static final Color MOVE = Color.GREEN;
+	public static final Color NOPASS = Color.WHITE;
+	public static final Color PLAYER1 = Color.RED;
+	public GameBoard myGame;
+	
+	public GameBoardComponent (GameBoard myGame)
+	{
+		this.myGame = myGame;
+	}
+	
+	/**
+	 * Draws lines that make up the grid
+	 * Draws the squares of colour to colour the spots on the grid.
+	 * @param g2 : Graphics2D
+	 */
+	public void setGrid (Graphics2D g2)
+	{	
+		//int player =-1;
+		int col = -1;
+		int row = -1;
+		int [][] myMove = new int[0][2];
+		Image image = new ImageIcon("img/queen.png").getImage();
+		
+		//Draw order
+		//	Squares
+		// 	Valid moves
+		//	Circles 
+		myGame.setSize(getWidth(), getHeight());
+		
+		if (myGame.getGridOn())	
+		{
+			for (int i = 0 ; i < myGame.getColumn(); i++)
+			{
+				for (int j = 0; j < myGame.getRow(); j++)
+				{
+					if (myGame.isTileNull(i, j))
+					{
+						g2.setColor(NOPASS);
+					}
+					else 
+					{						
+						g2.setColor(PASS);
+						
+						if (i == col && j == row )
+						{
+							g2.setColor(MOVE);
+						}
+						else 
+						{
+							for (int k = 0; k < myMove.length; k++)
+							{
+								if (myMove[k][0] == i && myMove[k][1] == j)
+								{
+									g2.setColor(MOVE);
+								}
+							}
+						}
+					}
+					
+					g2.fillRect(myGame.getX(i+1), myGame.getY(j+1), myGame.getWidth(), myGame.getHeight());
+					
+					if (!myGame.isTileNull(i, j) && myGame.getTileOwner(i,j))
+					{
+						g2.setColor(PLAYER1);
+						
+						g2.fillOval(myGame.getX(i+1), myGame.getY(j+1), myGame.getWidth(), myGame.getHeight());
+						
+						g2.drawImage(image, myGame.getX(i+1), myGame.getY(j+1), 
+								myGame.getWidth(), myGame.getHeight(), null);
+					}
+				}
+			}
+		}
+	}
+	
+	public void paintComponent (Graphics g)
+	{
+		Graphics2D g2 = (Graphics2D) g;
+		setGrid(g2);
+	}
+}
---
+++ src/gameboard/QueenBoard.java
@@ -0,0 +1,71 @@
+
+public class QueenBoard extends GameBoard
+{	
+	private static final int NUMBER_OF_QUEENS = 8;
+	
+	public QueenBoard (int[] queenPosition) throws IllegalArgumentException
+	{
+		row = 8; 
+		column = 8;
+		piece = new QueenPiece[8];
+		tiles = new Tile[column][row];
+			
+		gridOn = true;
+		center = true;
+		pieceMove = -1;
+		
+		setBoard(queenPosition);
+	}
+	
+	@Override
+	public void setBoard(Object position) throws IllegalArgumentException {
+		
+		int a = 0;
+		for (int i = 0; i < column; i ++)
+		{
+			for (int j = 0; j < row; j ++)
+			{
+				if (j%2 == a)
+				{					
+					tiles[j][i] = new Tile(false);
+				}
+			}
+			if (a == 0)
+				a = 1;
+			else
+				a = 0;
+		}
+		
+		int[] queenPosition = (int[])position;
+		int size = queenPosition.length;
+		
+		if (size > NUMBER_OF_QUEENS)
+		{
+			throw new IllegalArgumentException("Array size must be less than 8");
+		}
+		for(int i = 0; i < size ; i++)
+		{
+			QueenPiece newPos = new QueenPiece(queenPosition[i], i);
+			if(newPos.getRow() >= NUMBER_OF_QUEENS)
+			{
+				throw new IllegalArgumentException("Elements in array must be between 0 and 7");
+			}
+			
+			piece[i] = newPos;
+			tiles[queenPosition[i]][i] = new Tile(true);
+		}
+		
+	}
+
+	@Override
+	public boolean move(int player, int col, int row, int newCol, int newRow) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getLost() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
