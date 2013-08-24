46bf1c91d3735595ec220275c527390e8f7fe9bf
gnu_user <gsoc.student@gmail.com>
2013-08-23 21:47:46 -0400

    Fixed an issue where fixed mutation rates had no solutions

--- analysis/AggregateResults.R
+++ analysis/AggregateResults.R
@@ -379,7 +379,7 @@ for (nqueens_dir in nqueens_dirs)
     
     # Add the max generations to the number of solutions found for the last entry
     max_gen <- 10000000
-    for (mutation_rate in unique(summary_solution[, mutation]))
+    for (mutation_rate in unique(summary_solution[queen == nqueens, mutation]))
     {
         entry <- last(summary_solution[queen == nqueens & mutation == mutation_rate])
         
